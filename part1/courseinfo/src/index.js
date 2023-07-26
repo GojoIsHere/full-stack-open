6782eeedd2ba1f47a6f42d08c04a09220522c3b7
GojoIsHere <sus.thanet0726@gmail.com>
2023-07-26 07:11:15 +0545

    remodule with tooling

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+let container = document.getElementById("root");
+let root = createRoot(container);
+
+root.render(React.createElement(App));
