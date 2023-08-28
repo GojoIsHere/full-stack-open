c03273e65d037dd20435d45dcabca186f2627880
GojoIsHere <sus.thanet0726@gmail.com>
2023-08-28 19:23:07 +0545

    feat: added part3 and 4

--- part3/index.js
+++ part3/index.js
@@ -1,77 +1,113 @@
-console.log("hello world 1");
-
-const { response } = require("express");
-// const http = require('http');
-
-//using express:
 const express = require("express");
 const app = express();
-app.use(express.json()); // it converts the json data to object while posting
+const morgan = require("morgan");
+const cors = require("cors");
+const Phonebook = require("./models/phonebook");
+require("dotenv").config();
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
-app.get("/", (request, response) => {
-  response.send("<h1>hellow world</h1>");
+const PORT = process.env.PORT ? process.env.PORT : 3001;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("dist"));
+
+app.get("/", (req, res) => {
+  res.send("<h1>This is phonebook app.</h1>");
 });
-app.get("/api/notes/", (request, response) => {
-  response.json(notes).status(200).end();
-});
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  // console.log(id);
 
-  const note = notes.find((note) => {
-    // console.log(note.id, typeof note.id, id, typeof id, note.id === id);
-    return note.id === id;
+app.get("/api/persons", (req, res, next) => {
+  Phonebook.find({})
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+app.get("/api/persons/:id", (req, res, next) => {
+  const myId = req.params.id;
+  Phonebook.findById(myId)
+    .then((result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({ error: "requested data not found!" });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+app.get("/info", (req, res) => {
+  Phonebook.count().then((result) => {
+    const date = new Date();
+    res
+      .status(200)
+      .send(`<p>Phonebook has info for ${result} people<br />${date}</p>`);
   });
-  // console.log(note);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).send("index not avaliable").end();
+});
+
+app.post("/api/persons", (req, res, next) => {
+  const newObj = req.body;
+  console.log(newObj);
+  Phonebook.findOne({ name: newObj.name }).then((result) => {
+    if (result) {
+      res.status(403).json({ error: "name must be unique" });
+    } else {
+      if (newObj.name && newObj.number) {
+        const contact = new Phonebook(newObj);
+        contact
+          .save()
+          .then((result) => {
+            res.status(200).json(result);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      } else {
+        res.status(400).json({ error: "Missing content" });
+      }
+    }
+  });
+});
+app.delete("/api/persons/:id", (req, res, next) => {
+  const myId = req.params.id;
+  Phonebook.findByIdAndDelete(myId)
+    .then(() => {
+      res.status(204).send("Deletetion successful");
+    })
+    .catch((err) => next(err));
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const myId = req.params.id;
+  const updateData = req.body;
+
+  Phonebook.findByIdAndUpdate(myId, updateData, {
+    new: true,
+    runValidators: true,
+  })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+const errorHandler = (error, req, res, next) => {
+  if (error.name === "ValidationError") {
+    res.status(400).json({ error: error.message });
   }
+  next();
+};
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
 });
-app.delete("/api/notes/:id", (request, response) => {
-  const thisId = Number(request.params.id);
-  // console.log(id);
-
-  notes = notes.filter((note) => {
-    // console.log(note.id, typeof note.id, id, typeof id, note.id === id);
-    return note.id !== thisId;
-  });
-  response.status(204).send(`this note at id ${thisId} had been deleted`).end();
-});
-
-app.post("/api/notes", (request, response) => {
-  const myNewPost = request.body;
-  console.log("this is myNewPost", myNewPost);
-  myNewPost.id = notes.length + 1;
-  notes.push(myNewPost);
-  response.status(201).json(myNewPost);
-});
-
-// setting up server required through http
-// const app = http.createServer((request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'application/json' })
-//   response.end(JSON.stringify(notes))
-// })
-
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
